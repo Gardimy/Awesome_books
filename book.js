e24eb7920a7b0b5569f3290f397b7b9e11dd36fc
JuanDBta <juandavid_diaz@hotmail.com>
2023-05-01 15:59:45 -0500

    Fix linters and eslint errors

--- book.js
+++ book.js
@@ -1,19 +1,19 @@
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
 function addBook(title, author) {
-  books.push({ title: title, author: author });
+  books.push({ title, author });
   localStorage.setItem('books', JSON.stringify(books));
 }
 
 function removeBook(title) {
-  books = books.filter(book => book.title !== title);
+  books = books.filter((book) => book.title !== title);
   localStorage.setItem('books', JSON.stringify(books));
 }
 
 function displayBooks() {
   const bookList = document.getElementById('book-list');
   bookList.innerHTML = '';
-  books.forEach(book => {
+  books.forEach((book) => {
     const bookItem = document.createElement('div');
     bookItem.classList.add('book');
     bookItem.innerHTML = `<span>${book.title} by ${book.author}</span>`;
@@ -24,7 +24,7 @@ function displayBooks() {
 displayBooks();
 
 const addBookForm = document.getElementById('add-book-form');
-addBookForm.addEventListener('submit', event => {
+addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
@@ -34,23 +34,23 @@ addBookForm.addEventListener('submit', event => {
 });
 
 const removeBookForm = document.getElementById('remove-book-form');
-      const removeTitle = document.getElementById('remove-title');
-      books.forEach(book => {
-        const titleOption = document.createElement('option');
-        titleOption.value = book.title;
-        titleOption.innerText = book.title;
-        removeTitle.appendChild(titleOption);
-      });
-      removeBookForm.addEventListener('submit', event => {
-        event.preventDefault();
-        const selectedTitle = removeTitle.value;
-        removeBook(selectedTitle);
-        displayBooks();
-        removeTitle.innerHTML = '';
-        books.forEach(book => {
-          const titleOption = document.createElement('option');
-          titleOption.value = book.title;
-          titleOption.innerText = book.title;
-          removeTitle.appendChild(titleOption);
-        });
-      });
+const removeTitle = document.getElementById('remove-title');
+books.forEach((book) => {
+  const titleOption = document.createElement('option');
+  titleOption.value = book.title;
+  titleOption.innerText = book.title;
+  removeTitle.appendChild(titleOption);
+});
+removeBookForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const selectedTitle = removeTitle.value;
+  removeBook(selectedTitle);
+  displayBooks();
+  removeTitle.innerHTML = '';
+  books.forEach((book) => {
+    const titleOption = document.createElement('option');
+    titleOption.value = book.title;
+    titleOption.innerText = book.title;
+    removeTitle.appendChild(titleOption);
+  });
+});
