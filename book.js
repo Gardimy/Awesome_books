bfa0a141313719bdfc7eb846a5b08c27998c0ce9
Gardimy Charles <gardimycharles889@gmail.com>
2023-04-30 23:01:24 -0400

    Update book.js

--- book.js
+++ book.js
@@ -1,6 +1,5 @@
 let books = JSON.parse(localStorage.getItem('books')) || [];
 
-// Define the functions to add and remove books
 function addBook(title, author) {
   books.push({ title: title, author: author });
   localStorage.setItem('books', JSON.stringify(books));
@@ -10,3 +9,34 @@ function removeBook(title) {
   books = books.filter(book => book.title !== title);
   localStorage.setItem('books', JSON.stringify(books));
 }
+
+function displayBooks() {
+  const bookList = document.getElementById('book-list');
+  bookList.innerHTML = '';
+  books.forEach(book => {
+    const bookItem = document.createElement('div');
+    bookItem.classList.add('book');
+    bookItem.innerHTML = `<span>${book.title} by ${book.author}</span>`;
+    bookList.appendChild(bookItem);
+  });
+}
+
+displayBooks();
+
+const addBookForm = document.getElementById('add-book-form');
+addBookForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  addBook(title, author);
+  displayBooks();
+  addBookForm.reset();
+});
+
+const removeBookForm = document.getElementById('remove-book-form');
+const removeTitleSelect = document.getElementById('remove-title');
+books.forEach(book => {
+  const titleOption = document.createElement('option');
+  titleOption.value = '';
+  titleOption.innerText = '';
+ });
