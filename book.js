a060ad0781ecb52eb24040ab08eb152a5a2ccbbd
ibugithub <outsideworkibrahim@gmail.com>
2023-05-04 02:23:58 +0600

    Put All the code inside one class

--- book.js
+++ book.js
@@ -1,39 +1,40 @@
-const books = JSON.parse(localStorage.getItem('books')) || [];
-
 class Book {
-  constructor(title, author, id) {
+  constructor(title, author) {
     this.title = title;
     this.author = author;
-    this.id = id;
-  }
-}
-
-class Library {
-  constructor() {
-    this.books = books.map((book) => new Book(book.title, book.author, book.id));
+    this.id = Date.now();
   }
 
-  addBook(title, author) {
-    const id = Date.now();
-    const book = new Book(title, author, id);
-    this.books.push(book);
-    localStorage.setItem('books', JSON.stringify(this.books));
+   getAllBooks() {
+    return JSON.parse(localStorage.getItem('books')) || [];
   }
 
-  removeBook(id) {
-    const index = this.books.findIndex((book) => book.id === Number(id));
-    this.books.splice(index, 1);
-    localStorage.setItem('books', JSON.stringify(this.books));
+   addBook(title, author) {
+    const book = new Book(title, author);
+    const books = this.getAllBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
-  displayBooks() {
+   removeBook(id) {
+    const books = this.getAllBooks();
+    const index = books.findIndex((book) => book.id === Number(id));
+    if (index !== -1) {
+      books.splice(index, 1);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
+
+   displayBooks() {
+    const books = this.getAllBooks();
     const bookList = document.getElementById('book-list');
     bookList.innerHTML = '';
-    this.books.forEach((book) => {
+    books.forEach((book) => {
       const bookItem = document.createElement('div');
       bookItem.classList.add('book');
-      bookItem.innerHTML = `<div class="Book-title">${book.title} by ${book.author}<div><button class="remove-title" data-id="${book.id}">Remove</button></div></div>`;
+      bookItem.innerHTML = `${book.title} by ${book.author}<button class="remove-title" data-id="${book.id}">Remove</button>`;
       bookList.appendChild(bookItem);
+
       const removeButton = bookItem.querySelector('.remove-title');
       removeButton.addEventListener('click', (event) => {
         const { id } = event.target.dataset;
@@ -44,24 +45,24 @@ class Library {
   }
 }
 
-const library = new Library();
+const book = new Book();
+book.displayBooks();
 
-library.displayBooks();
 const addBookForm = document.getElementById('add-book-form');
 addBookForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
-  library.addBook(title, author);
-  library.displayBooks();
+  book.addBook(title, author);
+  book.displayBooks();
   addBookForm.reset();
 });
 
-const removeButtons = document.querySelectorAll('.remove-title');
-removeButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
+const bookList = document.getElementById('book-list');
+bookList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove-title')) {
     const { id } = event.target.dataset;
-    library.removeBook(id);
-    library.displayBooks();
-  });
+    book.removeBook(id);
+    book.displayBooks();
+  }
 });
