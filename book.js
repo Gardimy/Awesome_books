b865e1a77e125de7ccaae5de9d5825ec7575596f
ibugithub <outsideworkibrahim@gmail.com>
2023-05-05 01:10:21 +0600

    Add event listener to the nav

--- book.js
+++ book.js
@@ -61,3 +61,30 @@ bookList.addEventListener('click', (event) => {
     books.displayBooks();
   }
 });
+
+document.querySelector('#list').addEventListener('click', () => {
+  document.querySelector('.list_container').classList.add('dBlock');
+  document.querySelector('.list_container').classList.remove('dNone');
+  document.querySelector('.contact_section').classList.add('dNone');
+  document.querySelector('.contact_section').classList.remove('dBlock');
+  document.querySelector('.form_section').classList.add('dNone');
+  document.querySelector('.form_section').classList.remove('dBlock');
+});
+
+document.querySelector('#addBook').addEventListener('click', () => {
+  document.querySelector('.list_container').classList.add('dNone');
+  document.querySelector('.list_container').classList.remove('dBlock');
+  document.querySelector('.form_section').classList.add('dBlock');
+  document.querySelector('.form_section').classList.remove('dNone');
+  document.querySelector('.contact_section').classList.add('dNone');
+  document.querySelector('.contact_section').classList.remove('dBlock');
+});
+
+document.querySelector('#contact').addEventListener('click', () => {
+  document.querySelector('.contact_section').classList.add('dBlock');
+  document.querySelector('.contact_section').classList.remove('dNone');
+  document.querySelector('.form_section').classList.add('dNone');
+  document.querySelector('.form_section').classList.remove('dBlock');
+  document.querySelector('.list_container').classList.add('dNone');
+  document.querySelector('.list_container').classList.remove('Block');
+});
